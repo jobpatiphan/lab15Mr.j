f50375a43fc62d45fc12f52f33801c6c154c9c3e
Mr.j <patiphan_klinhom@cmu.ac.th>
2023-01-17 16:15:56 +0700

    LAB Complete!!! เเล้วครับจารย์

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,50 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double x[][N])
+{
+    for(int i=0;i<N;i++)
+    {
+        cout << "Row " << i+1 << ": ";
+        for(int j=0;j<N;j++)
+        {
+            cin >> x[i][j];
+        }
+    }
+}
+
+void findLocalMax(const double x[][N], bool B[][N])
+{
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<N;j++)
+        {
+            if(i==0 || i==N-1 || j==0 || j==N-1)
+            {
+                B[i][j] = false;
+            }
+            else if(x[i][j] >= x[i-1][j] && x[i][j] >= x[i+1][j] && x[i][j] >= x[i][j-1] && x[i][j] >= x[i][j+1])
+            {
+                B[i][j] = true;
+            }
+            else
+            {
+                B[i][j] = false;
+            }
+        }
+    }
+}
+
+void showMatrix(const bool B[][N])
+{
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<N;j++)
+        {
+            cout << B[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
